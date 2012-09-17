fb90751c4e557a2b15c3ee318b3a034542011984
Volpini <bvolpini@dc2.com.br>
2012-09-17 09:34:33 -0300

    Adicionada opcoes de configuracao do Switcher. Opcoes personalizaveis : texto das opcoes, classe dos botoes, default value e width do switcher.

--- js/switcher.js
+++ js/switcher.js
@@ -1,43 +1,60 @@
-jQuery.fn.switcher = function(){
-    return this.each(function(){
-		var switcher = $(this);
-		switcher.val(1);
-        switcher.wrap("<div class='switch-container' />");
-
-		var div_mae = switcher.parent();
-		div_mae.prepend("<div class='switch-background'><a href='' class='btn btn-success button-switch'>&nbsp;</a><p class='desc-switch desc-switch-on'>ON</p></div>");
-		var switch_btn = div_mae.find(".button-switch");
-		var desc_switch = div_mae.find(".desc-switch");
-		switcher.css({
-			"visibility": "hidden",
-			"marginTop": "-20px",
-			"marginLeft": "10px"
-		})
+(function($){
+	$.fn.switcher = function(settings){
 		
-		div_mae.toggle(function(){
-			switcher.attr("checked", true)
-			console.log(div_mae.width());
-			console.log(switch_btn.width());
-			switch_btn.animate({
-			      marginLeft: "46px" 
-			    }, "fast");
-			desc_switch.removeClass("desc-switch-on");
-			desc_switch.addClass("desc-switch-off");
-			desc_switch.html("OFF");
-			switch_btn.removeClass("btn-success");
-			switch_btn.addClass("btn-danger");
-
-		}, function(){
-			switcher.attr("checked", false)
-			switch_btn.animate({
-			      marginLeft: "0px"
-			    }, "fast");
-			desc_switch.removeClass("desc-switch-off");
-			desc_switch.addClass("desc-switch-on");
-			desc_switch.html("ON");
-			switch_btn.removeClass("btn-danger");
-			switch_btn.addClass("btn-success");
-		})
+		var defaultOptions = {
+			'onButtonClass': 'btn-sucess',
+			'offButtonClass' : 'btn-danger',
+			'onButtonText' : 'ON',
+			'offButtonText' : 'OFF',
+			'defaultChecked' : true,
+			'animationSpeed' : 'fast',
+			'width' : '80' 
+		};
 		
-    });
-};
+		if (settings){$.extend(defaultOptions, settings);}
+		
+		return this.each(function(){
+			var switcher = $(this);
+	        switcher.wrap("<div class='switch-container' />");
+	
+			var div_mae = switcher.parent();
+			if(defaultOptions.defaultChecked){
+				switcher.attr("checked", true);
+				div_mae.prepend("<div class='switch-background'><span class='btn "+defaultOptions.onButtonClass+" button-switch'>&nbsp;</span><p unselectable='on' class='desc-switch desc-switch-on'>"+defaultOptions.onButtonText+"</p></div>");
+			}else{
+				div_mae.prepend("<div class='switch-background'><span class='btn "+defaultOptions.offButtonClass+" button-switch'>&nbsp;</span><p unselectable='on' class='desc-switch desc-switch-off'>"+defaultOptions.offButtonText+"</p></div>");
+				div_mae.find(".button-switch").css("float", "right");
+			}
+			var switch_btn = div_mae.find(".button-switch");
+			var desc_switch = div_mae.find(".desc-switch");
+			switcher.css({
+				"visibility": "hidden",
+				"marginTop": "-20px",
+				"marginLeft": "10px",
+				"width" : defaultOptions.width
+			});
+			
+			$(".switch-background").css({"width" : defaultOptions.width});
+			
+			div_mae.click(function(e){								
+				if(switcher.attr("checked")){
+					switcher.attr("checked", false);
+					switch_btn.css("float", "right");
+					desc_switch.removeClass("desc-switch-on");
+					desc_switch.addClass("desc-switch-off");
+					desc_switch.html(defaultOptions.offButtonText);
+					switch_btn.removeClass(defaultOptions.onButtonClass);
+					switch_btn.addClass(defaultOptions.offButtonClass);
+				}else{
+					switcher.attr("checked", true)					
+					switch_btn.css("float", "left");
+					desc_switch.removeClass("desc-switch-off");
+					desc_switch.addClass("desc-switch-on");
+					desc_switch.html(defaultOptions.onButtonText);
+					switch_btn.removeClass(defaultOptions.offButtonClass);
+					switch_btn.addClass(defaultOptions.onButtonClass);					
+				}
+			});
+		});
+	};
+})(jQuery);
